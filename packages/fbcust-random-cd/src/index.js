c4edeab3312290a493f9b12cd58168381b18ad88
takashi ogura <imperfectimpact@gmail.com>
2025-02-07 06:29:42 +0000

    readme added

--- packages/fbcust-random-cd/src/index.js
+++ packages/fbcust-random-cd/src/index.js
@@ -1,12 +1,12 @@
 (() => {
-	const TARGET_FIELD_CD = "QRCodeData";
+  const TARGET_FIELD_CD = "targetFieldCode";
 
-	fb.events.form.mounted = [
-		(state) => {
-			const uuid = self.crypto.randomUUID();
-			state.record[TARGET_FIELD_CD].value = uuid;
+  fb.events.form.mounted = [
+    (state) => {
+      const uuid = self.crypto.randomUUID();
+      state.record[TARGET_FIELD_CD].value = uuid;
 
-			return state;
-		},
-	];
+      return state;
+    },
+  ];
 })();
--- packages/fbcust-random-cd/vite.config.ts
+++ packages/fbcust-random-cd/vite.config.ts
@@ -1,18 +1,13 @@
-import fs from "node:fs";
-import path, { dirname } from "node:path";
-import { fileURLToPath } from "node:url";
+import path from "node:path";
 import { defineConfig } from "vite";
 import baseConfig from "../../vite.config";
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 // Viteの設定
 export default defineConfig({
-	...baseConfig,
+  ...baseConfig,
 
-	server: {
-		https: baseConfig?.server?.https,
-		open: path.join("src", "index.js"),
-	},
+  server: {
+    https: baseConfig?.server?.https,
+    open: path.join("src", "index.js"),
+  },
 });
-
