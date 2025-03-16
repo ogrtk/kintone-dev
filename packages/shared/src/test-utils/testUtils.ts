63fb47517f519d858f4704694053021a94cdf833
takashi ogura <imperfectimpact@gmail.com>
2025-03-16 06:28:24 +0000

    spyUnhandledRejectionの廃止、suppressNoisyError追加

--- packages/shared/src/test-utils/testUtils.ts
+++ packages/shared/src/test-utils/testUtils.ts
@@ -1,20 +1,30 @@
 import { vi } from "vitest";
 
-export async function spyUnhandledRejection(
-  test: (spy: ReturnType<typeof vi.fn>) => Promise<void>,
-) {
-  // 例外メッセージを補足するspyを設定
-  const unhandledRejectionSpy = vi.fn();
-  const originalHandler = process.listeners("unhandledRejection")[0];
-  process.removeAllListeners("unhandledRejection");
-  process.on("unhandledRejection", unhandledRejectionSpy);
+// export async function spyUnhandledRejection(
+//   test: (spy: ReturnType<typeof vi.fn>) => Promise<void>,
+// ) {
+//   // 例外メッセージを補足するspyを設定
+//   const unhandledRejectionSpy = vi.fn();
+//   const originalHandler = process.listeners("unhandledRejection")[0];
+//   process.removeAllListeners("unhandledRejection");
+//   process.on("unhandledRejection", unhandledRejectionSpy);
 
-  // テスト実行
-  await test(unhandledRejectionSpy);
+//   // テスト実行
+//   await test(unhandledRejectionSpy);
 
-  // 元のリスナーを復元
-  process.removeAllListeners("unhandledRejection");
-  if (originalHandler) {
-    process.on("unhandledRejection", originalHandler);
-  }
+//   // 元のリスナーを復元
+//   process.removeAllListeners("unhandledRejection");
+//   if (originalHandler) {
+//     process.on("unhandledRejection", originalHandler);
+//   }
+// }
+
+export async function suppressNoisyError(testCase: () => void | Promise<void>) {
+  const consoleErrorSpy = vi
+    .spyOn(console, "error")
+    .mockImplementation(() => {});
+
+  await testCase();
+
+  consoleErrorSpy.mockRestore();
 }
